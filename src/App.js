071fd20909532e6e992a993b764d00d56c2deff4
Hà Văn Được <havanduoc.dev@gmail.com>
2022-08-25 10:24:34 +0700

    Template redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import CounterFeature from './features/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Template Redux</h1>
+        <BrowserRouter>
+          <Link to="/"><li>Home</li></Link>
+          <Link to="/products"><li>Products</li></Link>
+          <Routes>
+            <Route path='/products' element={<CounterFeature />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
